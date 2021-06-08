46a0566f46b18b038b970fb4e1738544573a9cfb
rohitd743 <rohitd743@gmail.com>
2021-06-08 08:30:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CarouselContainer from "./components/CarouselContainer";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./components/Home";
+import { AboutUs } from "./components/AboutUs";
+import { Project } from "./components/Register";
+import { Login } from "./components/Login";
+import { AppNavBar } from "./Common/AppNavBar";
+import {
+  Button,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppNavBar />
+
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+
+        <Route path="/about-us">
+          <AboutUs />
+        </Route>
+
+        <Route path="/project">
+          <Project />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+      <div>
+        {/* <h1 className="bg-dark text-light p-2 sticky-top d-flex justify-content-center ">
+        Welcome to Financial Rating
+      </h1> */}
+
+        <CarouselContainer />
+      </div>
+    </Router>
   );
 }
 
