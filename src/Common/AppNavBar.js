7d52ef00ca451c15ce95072bc302bdf4f8513d56
rohitd743 <rohitd743@gmail.com>
2021-06-10 21:15:58 +0530

    navbar updated.

--- src/Common/AppNavBar.js
+++ src/Common/AppNavBar.js
@@ -17,7 +17,7 @@ import PersonAddIcon from "@material-ui/icons/PersonAdd";
 export function AppNavBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="#home" className="text-warning">
+      <Navbar.Brand as={Link} to="/" className="text-warning">
         <img src={logo} alt="Logo" />
         <span className="h2" style={{ fontFamily: "cursive" }}>
           Your One Stop Financial Solution
@@ -60,7 +60,7 @@ export function AppNavBar() {
                 <PersonAddIcon></PersonAddIcon> User
               </Dropdown.Item>
               <Dropdown.Divider />
-              <Dropdown.Item as={Link} to="/orgregister">
+              <Dropdown.Item as={Link} to="/organizationupsert">
                 <AccountBalanceIcon></AccountBalanceIcon> Organization{" "}
               </Dropdown.Item>
             </Dropdown.Menu>
@@ -79,26 +79,104 @@ export function AppNavBar() {
   );
 }
 
-// export function UserNavBar() {
-//   return (
-//     <Navbar bg="dark" expand="lg" variant="dark">
-//       <Navbar.Brand as={Link} to="/">
-//         VRS APP
-//       </Navbar.Brand>
-//       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//       <Navbar.Collapse id="basic-navbar-nav">
-//         <Nav className="ml-auto">
-//           <Nav.Link as={Link} to="/vehiclebookinglist">
-//             Booking Details
-//           </Nav.Link>
-//           <Nav.Link as={Link} to="/vehiclebooking">
-//             Book Vehicles
-//           </Nav.Link>
-//           <Nav.Link as={Link} to="/user">
-//             User
-//           </Nav.Link>
-//         </Nav>
-//       </Navbar.Collapse>
-//     </Navbar>
-//   );
-// }
+/////////Organization Registeration window nav bar/////////////////////////////////////////////////////////////////////
+
+export function OrgUpsertNavBar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/" className="text-warning">
+        <img src={logo} alt="Logo" />
+        <span className="h2" style={{ fontFamily: "cursive" }}>
+          Your One Stop Financial Solution
+        </span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/home" className="h5 text-light">
+            <HomeIcon></HomeIcon> Home
+          </Nav.Link>
+
+          <Nav.Link as={Link} to="/organizationlist" className="h5 text-light">
+            <AccountBalanceIcon></AccountBalanceIcon> Organization List
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+////////////  user registration navbar///////////////
+export function UserRegNavBar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/" className="text-warning">
+        <img src={logo} alt="Logo" />
+        <span className="h2" style={{ fontFamily: "cursive" }}>
+          Your One Stop Financial Solution
+        </span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/home" className="h5 text-light">
+            <HomeIcon></HomeIcon> Home
+          </Nav.Link>
+
+          <Nav.Link
+            as={Link}
+            to="/login"
+            className="h5 btn btn-primary text-light "
+          >
+            Login
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+////////////////////////user login screen nav bar/////////////////////////
+
+export function UserLoginNavBar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/" className="text-warning">
+        <img src={logo} alt="Logo" />
+        <span className="h2" style={{ fontFamily: "cursive" }}>
+          Your One Stop Financial Solution
+        </span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/home" className="h5 text-light">
+            <HomeIcon></HomeIcon> Home
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+////////////  home navbar///////////////
+export function HomeNavBar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/" className="text-warning">
+        <img src={logo} alt="Logo" />
+        <span className="h2" style={{ fontFamily: "cursive" }}>
+          Your One Stop Financial Solution
+        </span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link as={Link} to="/home" className="h5 text-light">
+            <HomeIcon></HomeIcon> Home
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
